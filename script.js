a456c4bc46ea0b567d07cb417f6e84322769bd5f
ChAl76 <chornyalexander@gmail.com>
2024-12-06 16:08:34 +0200

    define scales

--- script.js
+++ script.js
@@ -10,4 +10,17 @@ fetch(url)
       padding = 50;
 
     const svg = d3.select('#svg').attr('width', width).attr('height', height);
+
+    const xScale = d3
+      .scaleTime()
+      .domain([
+        new Date(d3.min(dataset, (d) => d[0])),
+        new Date(d3.max(dataset, (d) => d[0])),
+      ])
+      .range([padding, width - padding]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataset, (d) => d[1])])
+      .range([height - padding, padding]);
   });
