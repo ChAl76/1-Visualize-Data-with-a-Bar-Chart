e8196d7e06f01333bbb525784d777f90019ebe3f
ChAl76 <chornyalexander@gmail.com>
2024-12-06 16:06:48 +0200

    define the SVG canvas

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ fetch(url)
   .then((response) => response.json())
   .then((data) => {
     const dataset = data.data;
+    const width = 850,
+      height = 450,
+      padding = 50;
+
+    const svg = d3.select('#svg').attr('width', width).attr('height', height);
   });
