d0f75ae4c42da7011f7204bbc95ff8ba490f842a
ChAl76 <chornyalexander@gmail.com>
2024-12-06 16:10:10 +0200

    create Axes

--- script.js
+++ script.js
@@ -23,4 +23,19 @@ fetch(url)
       .scaleLinear()
       .domain([0, d3.max(dataset, (d) => d[1])])
       .range([height - padding, padding]);
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg
+      .append('g')
+      .attr('id', 'x-axis')
+      .attr('transform', `translate(0, ${height - padding})`)
+      .call(xAxis);
+
+    svg
+      .append('g')
+      .attr('id', 'y-axis')
+      .attr('transform', `translate(${padding}, 0)`)
+      .call(yAxis);
   });
